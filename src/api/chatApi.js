d86ee964f865f66c559f43d2e168547adeba9ef5
rachitk05 <rachit.k23csai@nst.rishihood.edu.in>
2025-01-01 19:25:29 +0530

    chaiApi fix

--- src/api/chatApi.js
+++ src/api/chatApi.js
@@ -2,7 +2,7 @@
 
 export async function sendChatMessage(request) {
     try {
-        const response = await fetch('http://127.0.0.1:8000/chat', {
+        const response = await fetch('https://pi-backend-psoq.onrender.com/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
